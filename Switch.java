d7a95e6a530a2611b213f36ad466b653b9e6a6a2
elegantdiya <pamelagautam10@gmail.com>
2016-07-03 15:22:46 +0530

    Program on switch case added

---
+++ Switch.java
@@ -0,0 +1,24 @@
+class Switch
+{
+	public static void main(String args[])
+	{
+		int age;
+		age = 3;
+
+		switch(age)
+		{
+			case 1:
+				System.out.println("You can crawl");
+				break;
+			case 2:
+				System.out.println("You can talk");
+				break;
+			case 3:
+				System.out.println("You can get in trouble");
+				break;
+			default:
+				System.out.println("I don't know how old are you");
+				break;
+		}
+	}
+}
